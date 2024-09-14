020b2fa95effdc72f4dc5665d1f821719d9c97e2
alextrifan1 <alexandrutrifan17@gmail.com>
2024-09-14 20:31:35 +0300

    kinda works \(-_-)/

--- simple_solarsystem.py
+++ simple_solarsystem.py
@@ -1,12 +1,22 @@
-from solarsystem import Solar_system, Sun
+from solarsystem import Solar_system, Sun, Planet
 
 solar_system = Solar_system(width=1400, height=900)
 
 sun = Sun(solar_system, mass=10_000)
-
-sun.draw()
-
-#
-
-import turtle
-turtle.done()
+planets = (
+    Planet(
+        solar_system,
+        mass=1,
+        position=(-350, 0),
+        velocity=(0, 5),
+    ),
+    Planet(
+        solar_system,
+        mass=2,
+        position=(-270, 0),
+        velocity=(0, 7),
+    ),
+)
+while True:
+    solar_system.calculate_all_body_interactions()
+    solar_system.update_all()
--- solarsystem.py
+++ solarsystem.py
@@ -1,3 +1,4 @@
+import itertools
 import math
 import turtle
 
@@ -17,15 +18,23 @@ class Solar_system_body(turtle.Turtle):
         solar_system.add_body(self)
 
     def draw(self):
+        self.clear()
         self.dot(self.display_size)
 
+    def move(self):
+        self.setx(self.xcor() + self.velocity[0])
+        self.sety(self.ycor() + self.velocity[1])
 class Sun(Solar_system_body):
     def __init__(self, solar_system, mass, position = (0, 0), velocity = (0, 0)):
         super().__init__(solar_system, mass, position, velocity)
         self.color("yellow")
 
 class Planet(Solar_system_body):
-    ...
+    colors = itertools.cycle(["#FFB3BA", "#B2FFB3", "#BAE1FF"])  # Pastel shades
+    def __init__(self, solar_system, mass, position=(0, 0), velocity=(0, 0)):
+        super().__init__(solar_system, mass, position, velocity)
+        self.color(next(Planet.colors))
+
 class Solar_system:
     def __init__(self, width, height):
         self.solar_system = turtle.Screen()
@@ -39,5 +48,41 @@ class Solar_system:
         self.bodies.append(body)
 
     def remove_body(self, body):
+        body.clear()
         self.bodies.remove(body)
 
+    def update_all(self):
+        for body in self.bodies:
+            body.move()
+            body.draw()
+        self.solar_system.update()
+
+    @staticmethod
+    def accelerate_due_to_gravity(
+            first: Solar_system_body,
+            second: Solar_system_body,
+    ):
+        distance = max(first.distance(second), 1)  # Prevent division by zero
+        force = first.mass * second.mass / distance ** 2
+        angle = first.towards(second)
+        reverse = 1
+        for body in first, second:
+            acceleration = force/body.mass
+            acc_x = acceleration * math.cos(math.radians(angle))
+            acc_y = acceleration * math.sin(math.radians(angle))
+            body.velocity = (body.velocity[0] + (reverse * acc_x), body.velocity[1] + (reverse * acc_y))
+            reverse = -1
+    def check_collision(self, first, second):
+        if isinstance(first, Planet) and isinstance(second, Planet):
+            return
+        if first.distance(second) < first.display_size/2 + second.display_size/2:
+            for body in first, second:
+                if isinstance(body, Planet):
+                    self.remove_body(body)
+
+    def calculate_all_body_interactions(self):
+        bodies_copy = self.bodies.copy()
+        for idx, first in enumerate(bodies_copy):
+            for second in bodies_copy[idx + 1:]:
+                self.accelerate_due_to_gravity(first, second)
+                self.check_collision(first, second)
